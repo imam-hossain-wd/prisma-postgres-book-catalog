b086a71ce81c419e03d3aaadfa3157f07c0219ef
imam-hossain <zunayedemon6t9@gmail.com>
2023-09-12 20:59:08 +0600

    create user complete

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express, { Application, NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
 
 import cookieParser from 'cookie-parser';
+import router from './app/routes';
 
 const app: Application = express();
 
@@ -12,6 +13,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/api/v1', router)
 
 app.use('/', (req: Request, res: Response, next: NextFunction) => {
   res.status(httpStatus.NOT_FOUND).json({
