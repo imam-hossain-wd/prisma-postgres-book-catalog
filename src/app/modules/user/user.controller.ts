15aaaff3f5465b85f0877e80c626273c87042531
imam-hossain <zunayedemon6t9@gmail.com>
2023-09-12 21:14:38 +0600

    user crud operation complete

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,21 +1,64 @@
-import { RequestHandler } from "express";
-import catchAsync from "../../../shared/catchAsync";
-import { userService } from "./user.service";
-import sendResponse from "../../../shared/sendResponse";
-import httpStatus from "http-status";
+import { RequestHandler } from 'express';
+import catchAsync from '../../../shared/catchAsync';
+import { userService } from './user.service';
+import sendResponse from '../../../shared/sendResponse';
+import httpStatus from 'http-status';
 
-
-const createUser:RequestHandler = catchAsync(async(req, res)=> {
-    const data = req.body;
-    const result = await userService.createUser(data);
-    sendResponse(res, {
-        statusCode:httpStatus.OK,
-        success:true,
-        message: "user create successfully",
-        data: result
-    })
-})
+const createUser: RequestHandler = catchAsync(async (req, res) => {
+  const data = req.body;
+  const result = await userService.createUser(data);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'user create successfully',
+    data: result,
+  });
+});
+const getAllUsers: RequestHandler = catchAsync(async (req, res) => {
+  const result = await userService.getAllUsers();
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'user retrived successfully',
+    data: result,
+  });
+});
+const getSingleUser: RequestHandler = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await userService.getSingleUser(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'single user retrived successfully',
+    data: result,
+  });
+});
+const updateUser: RequestHandler = catchAsync(async (req, res) => {
+  const data = req.body;
+  const { id } = req.params;
+  const result = await userService.updateUser(id, data);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'user updated successfully',
+    data: result,
+  });
+});
+const deleteUser: RequestHandler = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await userService.deleteUser(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'user delete successfully',
+    data: result,
+  });
+});
 
 export const userController = {
-    createUser
-}
+  createUser,
+  getAllUsers,
+  getSingleUser,
+  updateUser,
+  deleteUser,
+};
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -8,7 +8,47 @@ const createUser = async(data:User): Promise<User> => {
     return result;
 }
 
+const getAllUsers = async(): Promise<User[]> => {
+    const result = await prisma.user.findMany();
+    return result;
+}
+
+
+const getSingleUser = async(id:string): Promise<User | null> => {
+    const result = await prisma.user.findUnique({
+        where: {
+            id
+        }
+    })
+    return result;
+}
+
+
+const updateUser = async(id:string,data: Partial<User>): Promise<User | null> => {
+    const result = await prisma.user.update({
+        where: {
+            id
+        },
+        data
+    });
+    return result
+}
+const deleteUser = async( id:string): Promise<User | null> => {
+    const result = await prisma.user.delete({
+        where: {
+            id
+        }
+    });
+    return result
+}
+
+
+
 
 export const userService = {
-    createUser
+    createUser,
+    getAllUsers,
+    getSingleUser,
+    updateUser,
+    deleteUser
 }
