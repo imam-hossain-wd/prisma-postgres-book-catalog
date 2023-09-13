0b41a8e4f2f4ef57ce08fa1c6ff3bc45f5d243f1
imam-hossain <zunayedemon6t9@gmail.com>
2023-09-13 21:01:25 +0600

    global error handler setup complete

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import cors from 'cors';
 import express, { Application, NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
-
 import cookieParser from 'cookie-parser';
 import router from './app/routes';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+
 
 const app: Application = express();
 
@@ -14,6 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1', router)
+app.use(globalErrorHandler)
+
 
 app.use('/', (req: Request, res: Response, next: NextFunction) => {
   res.status(httpStatus.NOT_FOUND).json({
